2bfdf75fdc6964f1603665b7889c0d45d3c952c0
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-02-25 20:31:59 -0300

    Atualizado Hello World

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -3,6 +3,16 @@
 
 int main()
 {
+    Alo();
     printf("Hello world!\n");
     return 0;
+}
+
+void Alo()
+{
+    int i=0,qtd=10;
+    for(i=1;i<qtd;i++)
+    {
+        printf("Hello World!\n");
+    }
 }
