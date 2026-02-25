24038ffd299b3676a7766e60d1cd2fba38611eea
FELIPE FURLAN FILHO <felipe.filho004@cs.unicid.edu.br>
2026-02-25 20:50:27 -0300

    Separado função 'Alo' em novo arquivo

---
+++ HelloWorld/Alo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void Alo()
+{
+    int i=0,qtd=10;
+    for(i=1;i<qtd;i++){
+        printf("Hello World!\n");
+    }
+}
--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Alo();
 int main()
 {
     Alo();
-    printf("Hello world!\n");
     return 0;
 }
 
-void Alo()
-{
-    int i=0,qtd=10;
-    for(i=1;i<qtd;i++)
-    {
-        printf("Hello World!\n");
-    }
-}
+
